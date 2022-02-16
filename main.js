d608b2d284a94b60f35ef0df9642edd3607a2e06
luismarin2510 <opraedu@.com>
2022-02-15 20:55:37 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+/*operadores de asignacion  */
+///// operador de igualdad /////
+let valor = "luis"; // asigna mi nombre ala variable valor
+let nombre;        // se asigna un variable vacia
+nombre=valor;     // se le asigna mi nombre a valor todo por =
+                 // = es la manera de decir que es igual o que equivale 
+console.log(nombre,valor);
+
+/* operador de entrada input  con ventana emergente */
+let x = prompt("numero 1 = x","solo numeros");
+let y = prompt("numero 2 = y","solo numeros");
+/* asignacion y operadores arimeticos  */
+// let x=14;
+// let y=20;
+let suma;
+let resta;
+let multiplicacion;
+let divicion;
+
+suma=new Number (x)+new Number (y);
+resta=x-y;
+multiplicacion=x*y;
+divicion=x/y;
+
+console.log(`%c Respuestas:`,"color:green; font-size:15px");
+console.log(`datos son x ${x} & y ${y}
+suma= ${suma}
+resta= ${resta}
+multiplicacion= ${multiplicacion}
+divicion= ${divicion}`);
