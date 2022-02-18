e648b8d6b96aef50ad6e6d5e7485d88cc4876607
luismarin2510 <opraedu@.com>
2022-02-17 19:09:14 -0500

    residuo

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 /* operador de division */
 let x = prompt("numero 1 = x","solo numeros");
 let y = prompt("numero 2 = y","solo numeros");
-let division;
-division=x/y;
+let residuo;
+residuo=x%y;
 console.log(`Los datos son x=${x} & y=${y}`);
-console.log(`repuesta = ${division}`);
+console.log(`repuesta = ${residuo}`);
