b426e684656b898058189a5750276a81bedb9bac
luismarin2510 <opraedu@.com>
2022-02-17 19:06:54 -0500

    division

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
-/* operador de multiplicacion */
+/* operador de division */
 let x = prompt("numero 1 = x","solo numeros");
 let y = prompt("numero 2 = y","solo numeros");
-let multiplicacion;
-multiplicacion=x*y;
+let division;
+division=x/y;
 console.log(`Los datos son x=${x} & y=${y}`);
-console.log(`repuesta = ${multiplicacion}`);
+console.log(`repuesta = ${division}`);
