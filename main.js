39974d14996ef9998aafefc1ab9ec824dd3ef04b
luismarin2510 <opraedu@.com>
2022-02-17 19:04:09 -0500

    multiplicacion

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
-/* operador de resta */
+/* operador de multiplicacion */
 let x = prompt("numero 1 = x","solo numeros");
 let y = prompt("numero 2 = y","solo numeros");
-let resta;
-resta=x-y;
+let multiplicacion;
+multiplicacion=x*y;
 console.log(`Los datos son x=${x} & y=${y}`);
-console.log(`repuesta = ${resta}`);
+console.log(`repuesta = ${multiplicacion}`);
